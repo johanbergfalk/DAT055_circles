e64914b7c8a41e9bad89c9a79be74cbca7b6c11d
johanbergfalk <johanbergfalk@outlook.com>
2022-01-31 16:24:07 +0100

    Blir det fel??

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("HEJ ROBERT");
+	    System.out.println("HEJ JÖRGEN");
         System.out.println("Dehär går ju!..");
         new TacoClass();
+
     }
 }
