76a9948b10834f87f61526e30544bb1c283e7e44
BobRobBob <robert.stefan.nilsson@gmail.com>
2022-01-31 16:23:07 +0100

    Third the charm

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("HEJ ROBERT");
+        System.out.println("Dehär går ju!..");
         new TacoClass();
     }
 }
