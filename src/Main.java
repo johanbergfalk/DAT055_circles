83b4a663bdbbdd48286b579278d49b9b875ea845
BobRobBob <robert.stefan.nilsson@gmail.com>
2022-02-01 16:14:30 +0100

    Pls work

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	    System.out.println("HEJ JÖRGEN");
         System.out.println("OSCARS rabarber");        //Är jag filip eller buildtools
         System.out.println("Dehär går ju!..");
-        // HEJ OSCAR, HEJ IGEN hehehehe hackerman
+        // HEJ OSCAR, HEJ IGEN hehehehe hackerman rob the man
         System.out.println("Det går bra för Filip");
         /*
 
