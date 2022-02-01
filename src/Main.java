322366ec85d866df65e3a10fc611d0ef73988f10
BuildTools <unconfigured@null.spigotmc.org>
2022-02-01 13:41:37 +0100

    HOT PANTS GENERATOR

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("HEJ JÖRGEN");
         System.out.println("Dehär går ju!..");
+        // HEJ OSCAR
         new TacoClass();
 
     }
