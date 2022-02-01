654c956a8593c303b01285d82a18b0b6a8383424
ErikHarEnFraga <rober.stefan.nilsson@gmail.com>
2022-02-01 16:20:11 +0100

    hackerman

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("OSCARS rabarber");        //Är jag filip eller buildtools
         System.out.println("Dehär går ju!..");
         // HEJ OSCAR, HEJ IGEN hehehehe hackerman rob the man
-        //Ni har glömt räkna på jordens gravitation
+        //Ni har glömt räkna på jordens gravitationqwdwqd
         System.out.println("Det går bra för Filip");
         /*
 
