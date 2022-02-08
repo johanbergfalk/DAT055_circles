0748b0a9266f68ee114670535d47fb0a8f669862
filip-svensson <fils@student.chalmers.se>
2022-02-08 09:36:32 +0100

    Loginclasses

---
+++ src/loginControl.java
@@ -0,0 +1,206 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+
+public class loginControl implements ActionListener {
+    loginModel model;
+    DBConnection conn;
+    JFrame window;
+    JPanel loginPanel;
+    String user;
+    JTextField username;
+    JPasswordField password;
+    JLabel loginError;
+    JPanel mainPanel;
+
+
+    public loginControl() throws SQLException, URISyntaxException {
+        conn = new DBConnection();
+        model = new loginModel();
+
+        window = new JFrame ("Circles");
+        window.setMinimumSize(new Dimension(500,300));
+        loginPanel = this.makeLoginPanel();
+        window.add(loginPanel);
+        window.setLocation(100,100);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+
+    public static void main(String[] args) throws SQLException, URISyntaxException {
+        new loginControl();
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String sourceClicked = e.getActionCommand();
+        if(username.getText() != ""){
+        switch(sourceClicked) {
+            case "Login":
+                byte[] h = conn.getHash(username.getText());
+                byte[] s = conn.getSalt(username.getText());
+                if(s != null){
+                if (Passwords.isExpectedPassword(password.getPassword(), s, h)) {
+                    this.user = username.getText();
+                    password.setText("");
+                    username.setText("");
+                    loginError.setVisible(false);
+                    this.mainPanel = this.makeMainPanel();
+                    loginPanel.setVisible(false);
+                    window.add(mainPanel);
+                } else {
+                    password.setText("");
+                    username.setText("");
+                    loginError.setVisible(true);
+                }
+                } else {
+                    password.setText("");
+                    username.setText("");
+                    loginError.setVisible(true);
+                }
+                break;
+            case "Register":
+                if(username.getText().length() >= 4 && password.getPassword().length >= 4) {
+                    byte[] sr = Passwords.getNextSalt();
+                    byte[] hr = Passwords.hash(password.getPassword(), sr);
+                    boolean success = conn.addUser(username.getText(), hr, sr);
+                    password.setText("");
+                    username.setText("");
+                    if (success) {
+                        JOptionPane.showMessageDialog(this.window, "You are now registered. Please log in.");
+                    } else {
+                        JOptionPane.showMessageDialog(this.window, "Username already taken.");
+                    }
+                    break;
+                } else {
+                    JOptionPane.showMessageDialog(this.window, "Username and password need to be atleast four charachters each.");
+                }
+        }
+        }
+        window.repaint();
+    }
+
+    private void terminate() throws SQLException {
+        conn.close();
+        System.exit(0);
+    }
+
+    private JPanel makeMainPanel(){
+        JPanel users = new JPanel();
+        users.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        String[] db = conn.getUsers();
+        for (String s : db) {
+            JLabel user = new JLabel(s);
+            users.add(user, gbc);
+        }
+        JScrollPane allUsers = new JScrollPane(users);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new GridLayout(2,1,0,0));
+        labelPanel.add(new JLabel("Logged in as: " + this.user));
+        labelPanel.add(new JLabel("All registered users so far: "));
+        mainPanel.add(labelPanel, BorderLayout.NORTH);
+        mainPanel.add(allUsers, BorderLayout.CENTER);
+        return mainPanel;
+    }
+
+    private JPanel makeLoginPanel(){
+
+        JPanel finishedPanel = new JPanel();
+        finishedPanel.setBackground(Color.WHITE);
+        finishedPanel.setLayout(new GridLayout(3,1,0,10));
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new GridLayout(2,1,3,3));
+        textPanel.setBackground(Color.WHITE);
+        JLabel welcome = new JLabel("Welcome to Circles");
+        welcome.setHorizontalAlignment(0);
+        welcome.setVerticalAlignment(JLabel.BOTTOM);
+        welcome.setFont(new Font("Arial Black",Font.BOLD,24));
+        welcome.setForeground(Color.decode("#C6E2FF"));
+
+
+        JLabel prompt = new JLabel ("Please login or register to continue:");
+        prompt.setHorizontalAlignment(0);
+        textPanel.add(welcome);
+        textPanel.add(prompt);
+        finishedPanel.add(textPanel);
+
+
+        JPanel userpass = new JPanel();
+        JPanel userpassFinal = new JPanel();
+        userpassFinal.setBackground(Color.WHITE);
+        userpassFinal.setLayout(new FlowLayout());
+        JPanel emptyu1 = new JPanel();
+        JPanel emptyu2 = new JPanel();
+        emptyu2.setBackground(Color.WHITE);
+        emptyu1.setBackground(Color.WHITE);
+        userpassFinal.add(emptyu1);
+        userpass.setLayout(new GridLayout(2,2,0,0));
+        userpass.setBackground(Color.WHITE);
+        JLabel uname = new JLabel("Username: ");
+        uname.setHorizontalAlignment(0);
+        JLabel pass = new JLabel("Password: ");
+        pass.setHorizontalAlignment(0);
+        username = new JTextField();
+        username.setColumns(15);
+        password = new JPasswordField();
+        password.setColumns(15);
+        userpass.add(uname);
+        userpass.add(username);
+        userpass.add(pass);
+        userpass.add(password);
+
+        userpassFinal.add(userpass);
+        userpassFinal.add(emptyu2);
+
+        finishedPanel.add(userpassFinal);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3,1,1,10));
+        buttonPanel.setBackground(Color.WHITE);
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(this);
+        JButton registerButton = new JButton("Register");
+        registerButton.addActionListener(this);
+        buttonPanel.add(loginButton);
+        buttonPanel.add(registerButton);
+        loginError = new JLabel("Error: Wrong username or password");
+        loginError.setForeground(Color.RED);
+        loginError.setHorizontalAlignment(0);
+        loginError.setVisible(false);
+        buttonPanel.add(loginError);
+        JPanel alignPanel = new JPanel();
+        JPanel empty1 = new JPanel();
+        empty1.setBackground(Color.WHITE);
+        JPanel empty2 = new JPanel();
+        empty2.setBackground(Color.WHITE);
+        alignPanel.setLayout(new BoxLayout(alignPanel,BoxLayout.X_AXIS));
+        alignPanel.add(empty1);
+        alignPanel.add(buttonPanel);
+        alignPanel.add(empty2);
+        finishedPanel.add(alignPanel);
+        return finishedPanel;
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/loginModel.java
@@ -0,0 +1,15 @@
+public class loginModel {
+
+    private String userName;
+    private String password;
+
+    public loginModel(){};
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
