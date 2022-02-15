25bc28a706d6522c83c87572cfe3a897300a75f1
racso99 <98709580+oscalars@users.noreply.github.com>
2022-02-15 15:42:39 +0100

    lagt till MainFrame klass, flyttat main

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 
-public class MainFrame {
+public class MainFrame extends JFrame{
 
+public MainFrame(String title){
+    this.setTitle(title);
 
-
-
-
+}
     public static void main(String[] args) throws SQLException, URISyntaxException {
         new loginControl();
     }
-
 }
