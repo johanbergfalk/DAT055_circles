5df6f0f18b936e1fc132584decf657284d31d1fa
johanbergfalk <johanbergfalk@outlook.com>
2022-02-18 16:53:33 +0100

    Api work with title

--- src/CircleDetailsPanel.java
+++ src/CircleDetailsPanel.java
@@ -1,26 +1,40 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class CircleDetailsPanel extends JPanel {
 
     LinkedList<Movie> moviesInCircle = new LinkedList<>();
 
-    Movie movie1 = new Movie("film1", 1, "description or genre", "1997", "url");
-    Movie movie2 = new Movie("film2", 2, "description or genre", "2004", "url");
-    Movie movie3 = new Movie("film2", 3, "description or genre", "2015", "url");
+    public CircleDetailsPanel(JFrame frame) throws IOException {
 
-    public CircleDetailsPanel(JFrame frame) {
+        Movie movie1 = new Movie("die hard 2");
+        Movie movie2 = new Movie("forrest gump");
+        Movie movie3 = new Movie("ett päron till farsa");
+        Movie movie4 = new Movie("catch me if you can");
+        Movie movie5 = new Movie("die hard 3");
+        Movie movie6 = new Movie("pulp fiction");
+        Movie movie7 = new Movie("titanic");
+        Movie movie8 = new Movie("dirty dancing");
+        Movie movie9 = new Movie("grease");
 
         moviesInCircle.add(movie1);
         moviesInCircle.add(movie2);
         moviesInCircle.add(movie3);
+        moviesInCircle.add(movie4);
+        moviesInCircle.add(movie5);
+        moviesInCircle.add(movie6);
+        moviesInCircle.add(movie7);
+        moviesInCircle.add(movie8);
+        moviesInCircle.add(movie9);
 
-        setBorder(BorderFactory.createTitledBorder("Oscars romance"));
         JPanel content = new JPanel();
+        setBorder(BorderFactory.createTitledBorder(new EmptyBorder(10,5,5,5), "Oscars Romance"));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
-        content.setPreferredSize(new Dimension(frame.getWidth() - 20, frame.getHeight() - 65));
 
         for(Movie m : moviesInCircle) {
             content.add(new MovieCard(m));
