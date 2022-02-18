7a36a3d29d5df3de3b05c1d0fbce35b4a7c03026
filip-svensson <fils@student.chalmers.se>
2022-02-18 08:50:27 +0100

    getHash, getSalt, registerUser

--- src/DatabaseConn.java
+++ src/DatabaseConn.java
@@ -70,26 +70,42 @@ public class DatabaseConn {
     }
     */
 
-    public static void registerUser(String username, byte[] hash, byte[] salt, Consumer<Boolean> result){
-        new Thread(() -> {
+    public static boolean registerUser(String username, byte[] hash, byte[] salt) {
             try {
                 PreparedStatement ps = getInstance().c.prepareStatement("INSERT INTO login VALUES (?,?,?)");
                 ps.setString(1, username);
                 ps.setBytes(2, hash);
                 ps.setBytes(3, salt);
                 ps.executeUpdate();
-                result.accept(true);
-            } catch (SQLException se){
-                result.accept(false);
+                return true;
+            } catch (SQLException se) {
+                return false;
             }
-        }).start();
     }
 
-    public static void getHash(String user){
-
+    public static byte[] getHash(String user){
+        try {
+            PreparedStatement ps = getInstance().c.prepareStatement("SELECT hash FROM login WHERE username = ?");
+            ps.setString(1, user);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getBytes(1);
+        } catch (SQLException se){
+            return null;
+        }
     }
 
-
+    public static byte[] getSalt(String user){
+        try {
+            PreparedStatement ps = getInstance().c.prepareStatement("SELECT salt FROM login WHERE username = ?");
+            ps.setString(1, user);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getBytes(1);
+        } catch (SQLException se){
+            return null;
+        }
+    }
 
 }
 
