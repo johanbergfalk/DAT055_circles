05a35608f8febdcb65f657f61a72a2229b253297
racso99 <98709580+oscalars@users.noreply.github.com>
2022-02-24 21:36:32 +0100

    Settingspanel added plus navigation and darkmode!

--- src/Settingspanel.java
+++ src/Settingspanel.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,53 +17,57 @@ public class Settingspanel extends JPanel implements ActionListener {
     private void draw(User u, MainFrame m){
 
         setLayout(new BorderLayout());
-        add(new NavigationBar(m, u, 1), BorderLayout.NORTH);
-        setBorder(BorderFactory.createTitledBorder(new EmptyBorder(10, 5, 5, 5), " "));
+        add(new NavigationBar(m, u, 4), BorderLayout.NORTH);
+        setBorder(BorderFactory.createTitledBorder (new EmptyBorder(10,5,5,5), "Settings"));
 
         Color textColor = u.getForegroundColor();
         setBackground(u.getBackgroundColor());
-        setBorder(BorderFactory.createEmptyBorder(32, 32, 32, 32));
-
-        JPanel paneltest = new JPanel();
-        paneltest.setBorder(new LineBorder(Color.BLUE));
-        paneltest.setLayout(new BoxLayout(paneltest, BoxLayout.Y_AXIS));
-        paneltest.setBackground(u.getBackgroundColor());
 
         JLabel header = new JLabel("Settings");
         header.setForeground(textColor);
         header.setFont(new Font("serif", Font.BOLD, 48));
-
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+        header.setVerticalAlignment(SwingConstants.TOP);
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.setLayout(new GridLayout(6,1,1,10));
         JButton settings = new JButton(u.getDarkMode() ? "Enable Light Mode" : "Enable Dark Mode");
         settings.addActionListener(this);
         buttonPanel.setBackground(u.getBackgroundColor());
         buttonPanel.add(header);
         buttonPanel.add(settings);
-        paneltest.add(buttonPanel);
 
-        add(paneltest, BorderLayout.CENTER);
+
+        JPanel alignPanel = new JPanel();
+        JPanel empty1 = new JPanel();
+        empty1.setBackground(u.getBackgroundColor());
+        JPanel empty2 = new JPanel();
+        empty2.setBackground(u.getBackgroundColor());
+        alignPanel.setLayout(new BoxLayout(alignPanel,BoxLayout.X_AXIS));
+        alignPanel.setBackground(u.getBackgroundColor());
+        alignPanel.add(empty1);
+        alignPanel.add(buttonPanel);
+        alignPanel.add(empty2);
+        add(alignPanel);
+
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String action = e.getActionCommand();
-
         switch (action) {
-            case "Enable Light Mode": {
+            case "Enable Light Mode" -> {
                 u.setDarkmode(false);
                 removeAll();
                 draw(u, m);
                 validate();
-                break;
             }
-            case "Enable Dark Mode": {
+            case "Enable Dark Mode" -> {
                 u.setDarkmode(true);
                 removeAll();
                 draw(u, m);
                 validate();
-                break;
             }
         }
     }
