532ecd2190c567251e760eb5772291b48254f005
johanbergfalk <johanbergfalk@outlook.com>
2022-02-28 16:09:12 +0100

    Made CircleDetailsPanel take a actual circle

--- src/CircleDetailsPanel.java
+++ src/CircleDetailsPanel.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 public class CircleDetailsPanel extends JPanel {
 
-    LinkedList<Movie> moviesInCircle = new LinkedList<>();
+    LinkedList<Movie> moviesInCircle;
 
     public CircleDetailsPanel(MainFrame frame, User user, Circle c) {
 
@@ -18,33 +18,18 @@ public class CircleDetailsPanel extends JPanel {
         titledBorder.setTitleColor(textColor);
         setBorder(titledBorder);
 
-        //setBorder(BorderFactory.createTitledBorder(new EmptyBorder(10,5,5,5), "Oscars Romance"));
-
-        Movie movie1 = new Movie("die hard 2");
-        Movie movie2 = new Movie("forrest gump");
-        Movie movie3 = new Movie("ett päron till farsa");
-        Movie movie4 = new Movie("catch me if you can");
-        Movie movie5 = new Movie("die hard 3");
-        Movie movie6 = new Movie("pulp fiction");
-        Movie movie7 = new Movie("titanic");
-        Movie movie8 = new Movie("dirty dancing");
-        Movie movie9 = new Movie("grease");
-
-        moviesInCircle.add(movie1);
-        moviesInCircle.add(movie2);
-        moviesInCircle.add(movie3);
-        moviesInCircle.add(movie4);
-        moviesInCircle.add(movie5);
-        moviesInCircle.add(movie6);
-        moviesInCircle.add(movie7);
-        moviesInCircle.add(movie8);
-        moviesInCircle.add(movie9);
-
         JPanel content = new JPanel();
         content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 
-        for(Movie m : moviesInCircle) {
-            content.add(new MovieCard(m));
+        moviesInCircle = DatabaseConn.getCircleMovies(c);
+
+        if(moviesInCircle != null) {
+            for(Movie m : moviesInCircle) {
+                content.add(new MovieCard(m));
+            }
+        }
+        else {
+            content.add(new JLabel("No movies has been added to this circle yet"));
         }
 
         JScrollPane scrollPane = new JScrollPane(content, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
