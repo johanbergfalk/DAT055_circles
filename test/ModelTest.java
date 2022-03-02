8abfc8fb73633ef292d07060c08cfcf7595f1d1a
johanbergfalk <johanbergfalk@outlook.com>
2022-03-02 18:23:46 +0100

    Add junit testing capability

---
+++ test/ModelTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ModelTest {
+    @Test
+    public void loginFailsWhenNonExistingUser() throws ExecutionException, InterruptedException {
+        Login login = new Login(new char[] {0}, "IDONTEXIST!!!");
+        Login.Result result = login.validateuser().get();
+
+        assertEquals(Login.Result.NO_SUCH_USER, result);
+    }
+}
