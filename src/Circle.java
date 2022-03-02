0614f1896f6cff779e5039b3509689da8aa6de53
BobRobBob <robert.stefan.nilsson@gmail.com>
2022-03-02 14:46:19 +0100

    Javadoc

--- src/Circle.java
+++ src/Circle.java
@@ -2,8 +2,9 @@ import java.util.Date;
 import java.util.LinkedList;
 
 /**
- * Everything needed for creating a circle
- *
+ * Stores all content of a circle
+ * @author Robert Nilsson
+ * @version 2022-03-02
  */
 
 public class Circle {
@@ -16,6 +17,9 @@ public class Circle {
     private Date stopTime;
     private float score = 0;
 
+    /**
+     * Empty constructor used in DatabaseConn
+     */
     public Circle(){
 
     }
@@ -40,70 +44,139 @@ public class Circle {
     }
 //----Getters and setters------------------------------------------
 
+    /**
+     *
+     * @return Circle unique id
+     */
     public int getId(){
         return this.id;
     }
 
+    /**
+     *
+     * @param id Circle id, only to be used by DatabaseConn!
+     */
     public void setId(int id){
         this.id = id;
     }
 
+    /**
+     *
+     * @return Circle name
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     *
+     * @param name Circle name
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     *
+     * @return username of Circle creator
+     */
     public String getCreator(){
         return this.creator;
     }
 
+    /**
+     *
+     * @param creator username of Circle creator
+     */
     public void setCreator(String creator){
         this.creator = creator;
     }
 
+    /**
+     *
+     * @return a LinkedList of all circle members, including creator
+     */
     public LinkedList<String> getMembers(){
         return this.members;
     }
 
+    /**
+     *
+     * @param members add a LinkedList with members
+     */
     public void setMembers(LinkedList<String> members){
         this.members = members;
     }
 
+    /**
+     *
+     * @param m add user as member to a circle
+     */
     public void addMember(String m){
         this.members.add(m);
     }
 
+    /**
+     *
+     * @return circle description
+     */
     public String getDescription(){
         return this.description;
     }
 
+    /**
+     *
+     * @param description set circle description
+     */
     public void setDescription(String description){
         this.description = description;
     }
 
+    /**
+     *
+     * @return the date of when the circle start
+     */
     public Date getStartTime(){
         return this.startTime;
     }
 
+    /**
+     *
+     * @param startTime date when the circle start
+     */
     public void setStartTime(Date startTime){
         this.startTime = startTime;
     }
 
+    /**
+     *
+     * @return date when the circle ends
+     *
+     */
     public Date getStopTime(){
         return this.stopTime;
     }
 
+    /**
+     *
+     * @param stopTime date when the circle ends
+     */
     public void setStopTime(Date stopTime){
         this.stopTime = stopTime;
     }
 
+    /**
+     *
+     * @return total score of the circle
+     */
     public float getScore(){
         return this.score;
      }
 
+    /**
+     *
+     * @param score total score of the circle
+     */
     public void setScore(float score){
         this.score = score;
      }
