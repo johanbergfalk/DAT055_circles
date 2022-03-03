8393827c1574cc8ccab3f80beeec26cdccb10976
filip-svensson <fils@student.chalmers.se>
2022-03-03 19:40:50 +0100

    Nog bättre Oscar skriver testen för settings och register. Han har bättre koll på klasserna.

---
+++ test/SettingsTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SettingsTest {
+
+    @Test
+    void test() throws SQLException {
+        User user = new User("To_Be_Removed");
+        char[] password = {4,5,6};
+        byte[] salt = Passwords.getNextSalt();
+        byte[] hash = Passwords.hash(password, salt);
+        DatabaseConn.registerUser("To_Be_Removed", hash, salt);
+
+
+        //Actual test:
+        char[] newPassword = {1,2,3};
+        Settings settings = new Settings(password, newPassword, "To_Be_Removed", newPassword);
+        assertEquals(Settings.Result.OK, settings.changepass());
+
+        DatabaseConn.removeTestUser(user);
+
+
+    }
+}
